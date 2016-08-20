8c94eb6a9bfee2dcbbd7297dcd76b6081055b51a
Taro Minowa <higepon@gmail.com>
2016-08-19 21:37:49 -0700

    uitable view.

--- HabitsDieHard/ViewController.swift
+++ HabitsDieHard/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var taskTableView: UITableView!
+    private let tasks = ["Hello", "World"]
+    private let taskCellIdentifier = "taskCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        taskTableView.delegate = self
+        taskTableView.dataSource = self
+        taskTableView.reloadData()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell: UITableViewCell? = tableView.dequeueReusableCellWithIdentifier(taskCellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: taskCellIdentifier)
+        }
+        cell!.textLabel?.text = tasks[indexPath.row]
+        return cell!
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
