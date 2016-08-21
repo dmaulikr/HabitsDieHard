543ce2cf7f34477c669c5b32c3a3158d9f736d30
Taro Minowa <higepon@gmail.com>
2016-08-21 09:47:35 -0700

    Add border to the cells

--- HabitsDieHard/WeeklyTableViewCell.swift
+++ HabitsDieHard/WeeklyTableViewCell.swift
@@ -16,17 +16,19 @@ class WeeklyTableViewCell: UITableViewCell {
     @IBOutlet weak var fridayView: UIView!
     @IBOutlet weak var saturdayView: UIView!
     @IBOutlet weak var sundayView: UIView!
+    let borderColor = UIColor.grayColor().CGColor
+    let borderWidth: CGFloat = 0.5
+    var weekDayViews: [UIView] = []
 
     override func awakeFromNib() {
         super.awakeFromNib()
         let selector = #selector(WeeklyTableViewCell.dayTapped)
-        mondayView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: selector))
-        tuesdayView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: selector))
-        wednesdayView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: selector))
-        thursdayView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: selector))
-        fridayView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: selector))
-        saturdayView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: selector))
-        sundayView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: selector))
+        weekDayViews = [mondayView, tuesdayView, wednesdayView, thursdayView, fridayView, saturdayView, sundayView]
+        weekDayViews.forEach { (view) in
+            view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: selector))
+            view.layer.borderWidth = borderWidth
+            view.layer.borderColor = borderColor
+        }
     }
 
     func dayTapped(gestureRecognizer: UIGestureRecognizer) {
